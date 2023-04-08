a604c95dac8c910db583c6101a5c7c52c587e5a7
Antonio Cheong <acheong@student.dalat.org>
2023-04-08 12:33:11 +0800

    fake system messages

--- internal/chatgpt/convert.go
+++ internal/chatgpt/convert.go
@@ -7,6 +7,10 @@ import (
 func ConvertAPIRequest(api_request typings.APIRequest) typings.ChatGPTRequest {
 	chatgpt_request := typings.NewChatGPTRequest()
 	for _, api_message := range api_request.Messages {
+		if api_message.Role == "system" {
+			api_message.Role = "user"
+			api_message.Content = "system: " + api_message.Content
+		}
 		chatgpt_request.AddMessage(api_message.Role, api_message.Content)
 	}
 	return chatgpt_request
