f87a3b3f3a56fba35b2a2f135d5634d4b3df4968
Antonio Cheong <acheong@student.dalat.org>
2023-04-08 12:38:59 +0800

    unnecessary tag

--- internal/chatgpt/convert.go
+++ internal/chatgpt/convert.go
@@ -9,7 +9,6 @@ func ConvertAPIRequest(api_request typings.APIRequest) typings.ChatGPTRequest {
 	for _, api_message := range api_request.Messages {
 		if api_message.Role == "system" {
 			api_message.Role = "user"
-			api_message.Content = "system: " + api_message.Content
 		}
 		chatgpt_request.AddMessage(api_message.Role, api_message.Content)
 	}
