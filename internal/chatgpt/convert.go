4d65eba605555447dad1056dfb7c460a5b06cdf4
Antonio <acheong@student.dalat.org>
2023-04-24 17:48:44 +0800

    extra tab

--- internal/chatgpt/convert.go
+++ internal/chatgpt/convert.go
@@ -8,7 +8,7 @@ func ConvertAPIRequest(api_request typings.APIRequest) typings.ChatGPTRequest {
 	chatgpt_request := typings.NewChatGPTRequest()
 	for _, api_message := range api_request.Messages {
 		if api_message.Role == "system" {
-			api_message.Role = "critic	"
+			api_message.Role = "critic"
 		}
 		chatgpt_request.AddMessage(api_message.Role, api_message.Content)
 	}
