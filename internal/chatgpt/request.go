1cd16a5d4a9ec938689df0fee8b58d108885d3b3
Antonio <acheong@student.dalat.org>
2023-05-01 16:39:42 +0800

    swap endpoint

--- internal/chatgpt/request.go
+++ internal/chatgpt/request.go
@@ -67,7 +67,7 @@ func SendRequest(message typings.ChatGPTRequest, access_token string) (*http.Res
 		client.SetProxy(proxies[random_int(0, len(proxies)-1)])
 	}
 
-	apiUrl := "https://ai.fakeopen.com/api/conversation"
+	apiUrl := "https://bypass.churchless.tech/conversation"
 	if API_REVERSE_PROXY != "" {
 		apiUrl = API_REVERSE_PROXY
 	}
