30ed66ec3b2b780b4ff80a5344f4b0a5f866232b
Antonio <acheong@student.dalat.org>
2023-05-03 17:29:45 +0800

    3rd party

--- main.go
+++ main.go
@@ -1,45 +1,123 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"os"
+
+	http "github.com/bogdanfinn/fhttp"
+	tls_client "github.com/bogdanfinn/tls-client"
 
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 )
 
-var HOST string
-var PORT string
-
-func init() {
-	HOST = os.Getenv("SERVER_HOST")
-	PORT = os.Getenv("SERVER_PORT")
-	if HOST == "" {
-		HOST = "127.0.0.1"
+var (
+	jar     = tls_client.NewCookieJar()
+	options = []tls_client.HttpClientOption{
+		tls_client.WithTimeoutSeconds(360),
+		tls_client.WithClientProfile(tls_client.Chrome_112),
+		tls_client.WithNotFollowRedirects(),
+		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
 	}
-	if PORT == "" {
-		PORT = "8080"
-	}
-}
+	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+	http_proxy = os.Getenv("http_proxy")
+)
 
 func main() {
-	router := gin.Default()
 
-	router.Use(cors)
+	if http_proxy != "" {
+		client.SetProxy(http_proxy)
+		println("Proxy set:" + http_proxy)
+	}
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "9090"
+	}
+	handler := gin.Default()
+	handler.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
 	})
 
-	admin_routes := router.Group("/admin")
-	admin_routes.Use(adminCheck)
+	handler.POST("/v1/chat/completions", proxy)
 
-	/// Admin routes
-	admin_routes.PATCH("/password", passwordHandler)
-	admin_routes.PATCH("/tokens", adminCheck, tokensHandler)
-	/// Public routes
-	router.OPTIONS("/v1/chat/completions", optionsHandler)
-	router.POST("/v1/chat/completions", nightmare)
-	endless.ListenAndServe(HOST+":"+PORT, router)
+	gin.SetMode(gin.ReleaseMode)
+	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handler)
+}
+
+func proxy(c *gin.Context) {
+	// Read body
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	// Parse as json
+	var jsonBody map[string]interface{}
+	err = json.Unmarshal(body, &jsonBody)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Check if "max_tokens" is set and if set to nil
+	if _, ok := jsonBody["max_tokens"]; !ok {
+
+	} else if jsonBody["max_tokens"] == nil {
+		// Remove "max_tokens" from json
+		delete(jsonBody, "max_tokens")
+	}
+
+	request_body, err := json.Marshal(jsonBody)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	var url string
+	var request_method string
+	var request *http.Request
+	var response *http.Response
+
+	url = "https://api.jeeves.ai/generate/v3/chat"
+	request_method = c.Request.Method
+
+	request, err = http.NewRequest(request_method, url, bytes.NewReader(request_body))
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	request.Header.Set("Host", "api.jeeves.ai")
+	request.Header.Set("Origin", "https://jeeves.ai/")
+	request.Header.Set("Connection", "keep-alive")
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Keep-Alive", "timeout=360")
+	request.Header.Set("Authorization", c.Request.Header.Get("Authorization"))
+	request.Header.Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
+	request.Header.Set("sec-ch-ua-mobile", "?0")
+	request.Header.Set("sec-ch-ua-platform", "\"Linux\"")
+	request.Header.Set("sec-fetch-dest", "empty")
+	request.Header.Set("sec-fetch-mode", "cors")
+	request.Header.Set("sec-fetch-site", "same-origin")
+	request.Header.Set("sec-gpc", "1")
+	request.Header.Set("user-agent", user_agent)
+
+	response, err = client.Do(request)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	defer response.Body.Close()
+	c.Header("Content-Type", response.Header.Get("Content-Type"))
+	// Get status code
+	c.Status(response.StatusCode)
+	c.Stream(func(w io.Writer) bool {
+		// Write data to client
+		io.Copy(w, response.Body)
+		return false
+	})
+
 }
