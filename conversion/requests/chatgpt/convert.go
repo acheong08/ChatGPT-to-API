3fb4afed4dbee8e0e644248246f82a0caa0634c2
Antonio <acheong@student.dalat.org>
2023-06-17 11:15:02 +0800

    use mobile by default

--- conversion/requests/chatgpt/convert.go
+++ conversion/requests/chatgpt/convert.go
@@ -30,6 +30,9 @@ func ConvertAPIRequest(api_request official_types.APIRequest) chatgpt_types.Chat
 		chatgpt_request.Model = api_request.Model
 		chatgpt_request.ArkoseToken = generate_random_hex(17) + "|r=ap-southeast-1|meta=3|meta_width=300|metabgclr=transparent|metaiconclr=%23555555|guitextcolor=%23000000|pk=35536E1E-65B4-4D96-9D97-6ADB7EFF8147|at=40|sup=1|rid=" + strconv.Itoa(randint(1, 99)) + "|ag=101|cdn_url=https%3A%2F%2Ftcr9i.chat.openai.com%2Fcdn%2Ffc|lurl=https%3A%2F%2Faudio-ap-southeast-1.arkoselabs.com|surl=https%3A%2F%2Ftcr9i.chat.openai.com|smurl=https%3A%2F%2Ftcr9i.chat.openai.com%2Fcdn%2Ffc%2Fassets%2Fstyle-manager"
 	}
+	if api_request.Model == "gpt-4" {
+		chatgpt_request.Model = "gpt-4-mobile"
+	}
 	if api_request.PluginIDs != nil {
 		chatgpt_request.PluginIDs = api_request.PluginIDs
 		chatgpt_request.Model = "gpt-4-plugins"
